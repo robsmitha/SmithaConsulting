dcb9dd5673ac345a3324727fd336d9daed9051c3
robsmitha <robsmitha94@gmail.com>
2019-05-28 19:50:56 -0400

    Add project files.

---
+++ ROD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ROD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
